26f11bb4a572b325ac579d96f8708b51cdcdb118
Rajeshgouda-143 <rajeshgoudakumar07@gmail.com>
2026-09-25 15:50:49 +0530

    datatypes-summery

---
+++ 01_basics/05_datatypes-summery.js
@@ -0,0 +1,31 @@
+//1. Primitive
+// 7 types : String, 
+/* Number, 
+ Boolean,
+ null, 
+ undefined, 
+ bigInt, 
+ Symbol
+ */
+
+ //2.Reference (Non Primitive)
+ // Array,  Objects, Functions 
+
+
+/* javascipt follows dynamic typed language it means there is no need to declared the datatype , based on the value it will assigned the datatype  */
+
+//array
+const heros = ["Saktiman","hagraj","donga"]
+
+//object
+let myObj = {
+    name:"rajesh",
+    age:22,
+}
+
+//function
+
+const myFunction = function(){
+    console.log("hello world!");
+}
+
